6db23d67273cf16597f5672dfd41b39703d4a6e0
alessiafabbioni <alessiafabbioni@gmail.com>
2023-12-05 16:54:56 +0100

    fix append header

--- main.js
+++ main.js
@@ -114,6 +114,8 @@ posts.forEach(post => {
 
         postHeader.appendChild(postMeta);
 
+
+});
 /*Milestone 2 - Se clicchiamo sul tasto “Mi Piace” cambiamo il colore al testo del 
 bottone e incrementiamo il counter dei likes relativo. Salviamo in un
 secondo array gli id dei post ai quali abbiamo messo il like.*/
